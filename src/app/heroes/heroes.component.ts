005f471e464752edc1f6854fd2c5a373dde44b1a
silvain ndongmo <decozar457@gmail.com>
2020-10-08 10:16:59 -0400

    second commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,10 +10,10 @@ import {HEROES} from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 /*Hero = 'spiderman';*/
 heroes = HEROES;
-hero: Hero = {
-  id: 1,
-  name: 'spiderman'
-};
+selectedHero: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
   constructor() { }
 
   ngOnInit(): void {
