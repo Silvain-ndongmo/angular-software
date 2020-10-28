5b7f78729ee80a6e1c72c27f2e2fa4b9647ca857
silvain ndongmo <decozar457@gmail.com>
2020-10-28 11:35:14 -0400

    seventh commit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,7 +29,12 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
-  goBack(): void {
+ goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+    .subscribe(() => this.goBack());
   }
 }
